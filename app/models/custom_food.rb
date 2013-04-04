60f5c3973374eb22b26f4415fcc29102c4e98b62
Nick <adiakritos@gmail.com>
2013-04-04 14:34:52 -0400

    get custom foods to work with users

--- app/models/custom_food.rb
+++ app/models/custom_food.rb
@@ -1,4 +1,3 @@
 class CustomFood < Food
   belongs_to :user
-  attr_accessible :user_id
 end                            
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404175951) do
+ActiveRecord::Schema.define(:version => 20130404183134) do
 
   create_table "foods", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130404175951) do
     t.string   "type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "meal_foods_meals", :force => true do |t|
